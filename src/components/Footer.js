dd3b81fec7965aabafef3a9351f0f07015784842
krushnabiradar <krushnabiradar1995@.com>
2022-09-30 15:17:17 +0530

    ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,11 @@ import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
+
   return (
     <Container fluid className="footer">
       <Row>
-        <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Krushna Biradar</h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year}</h3>
-        </Col>
-        <Col md="4" className="footer-body">
+        <Col md="4" className="footer-body col-md-12">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Krushna's Resume.pdf";
+import pdf from "../../Assets/Krushna's Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -40,7 +40,7 @@ function ResumeNew() {
             &nbsp;Download CV
           </Button>
         </Row>
-        
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <p>You can download my resume Directly if it fails to load.</p>
         </Row>
