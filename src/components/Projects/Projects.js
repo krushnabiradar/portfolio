0e7d1c61addda7cdb93990497a965165ea8dd544
krushnabiradar <krushnabiradar1995@.com>
2022-09-30 18:25:47 +0530

    added projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
+import Student from "../../Assets/Projects/Student.png";
 import chatify from "../../Assets/Projects/chatify.png";
+import Admin from "../../Assets/Projects/Admin.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -15,6 +17,26 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Admin}
+              isBlog={false}
+              title="Admin dashbord using react."
+              description="Admit dashbord using react"
+              ghLink="https://github.com/krushnabiradar/admin-dashboard-reactTask-main"
+              demoLink="https://admin-dashboard-reacttask-main.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Student}
+              isBlog={false}
+              title="Student teacher management."
+              description="This web is build by using react in which we can add users, see user profiles and we can set the type of user as student/teacher"
+              ghLink="https://github.com/krushnabiradar/social-media-client"
+              demoLink="https://student-teacher-management-by-kris.netlify.app/"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
